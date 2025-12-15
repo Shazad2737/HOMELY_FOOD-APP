ade9e066c9a72218b43fd9f2340287a1eda78f08
Shazad2737 <shazad2737@gmail.com>
2025-12-15 21:20:38 +0530

    feat: Initialize development environment with a dedicated app entry point and generated plugin registration files.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,9 +4,9 @@ import 'package:homely_app/bootstrap.dart';
 
 Future<void> main() async {
   await bootstrap(() {
-    // final api = HomelyApi(baseUrl: 'http://localhost:4000');
+    final api = HomelyApi(baseUrl: 'http://localhost:4000');
     // final api = HomelyApi(baseUrl: 'http://192.168.1.5:5000');
-    final api = HomelyApi(baseUrl: 'https://homely-app.chappan.me');
+    // final api = HomelyApi(baseUrl: 'https://homely-app.chappan.me');
     return App(api: api, releaseMode: ReleaseMode.development);
   });
 }
